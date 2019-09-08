01c77c1738a32bd17c075bcbd1c14b0b62dd631c
Kreautz <42375679+Kreautz@users.noreply.github.com>
2019-09-08 23:32:59 +0700

    db log

---
+++ database/migrations/2019_09_08_162847_activity_log.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ActivityLog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activitylog', function (Blueprint $table) {
+            $table->increments('id_activitylog');
+            $table->date('tgl');
+			$table->time('jam');
+			$table->text('detail');
+            $table->text('note');
+            $table->text('izin');
+			$table->integer('teacher_id')->unsigned()->nullable();
+			$table->integer('kelas_id')->unsigned()->nullable();
+            $table->foreign('teacher_id')->references('id_teacher')->on('mapel')->onDelete('cascade');
+			$table->foreign('kelas_id')->references('id_kelas')->on('kelas')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('activitylog');
+    }
+}
